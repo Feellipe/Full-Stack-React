e6228ec64eb048419414d00d8f5689f947219d82
Felipe Cavalcanti <fel.cavalcanti@hotmail.com>
2018-05-28 14:45:53 -0300

    Teste

--- src/Components/video_detail.js
+++ src/Components/video_detail.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const VideoDetail = ({video}) =>{
 	const videoId = video.id.videoId;
-	const url = `https://www.youtube.com/embed${ videoId}`;
-
+	const url = `https://www.youtube.com/embed${videoId}`;
+//Testando
 	return (
 		<div className="video-detail col-md-8">
 			<div className="embed-responsive embed-responsive-16by9">
